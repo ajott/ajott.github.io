d1d0ce3dcdba63a599ea67f85ff7355292c0b383
ajott <ajott@mtu.edu>
2017-08-08 10:52:42 -0400

    Hangman: One guess
    Whether clicking or typing, you cannot get a strike for guessing the
    same letter more than once.

--- hangman/main.js
+++ hangman/main.js
@@ -3,7 +3,8 @@ var newWord = '';
 var over = false;
 var strikes = 0;
 var bodyParts = ["hangHead", "hangLArm", "hangBody", "hangRArm", "hangLLeg", "hangRLeg"];
-var version = "0.0.3 Typist";
+var version = "0.0.5 Typist";
+var guessedArray = [];
 
 function buildLetters() {
   for (var j = 0; j < 13; j++) {
@@ -17,45 +18,57 @@ function buildLetters() {
   }
 }
 
-function guess(letter) {
-  document.getElementById(letter).setAttribute("style", "opacity:0;");
-  var guessed = [];
-  var gotOne = false;
-  for (i = 0; i < newWord.length; i++) {
-    if (letter === newWord[i]) {
-      document.getElementById(i).setAttribute("style", "color:black;");
-      gotOne = true;
-    }
-  }
-  if (!gotOne) {
-    document.getElementById(bodyParts[strikes]).setAttribute("style", "color:black;");
-    strikes += 1;
-  }
-  for (j = 0; j < newWord.length; j++) {
-    if (!document.getElementById(j).hasAttribute("style")) {
-      guessed.push(false);
-    }
-  }
-  if (guessed.length === 0) {
-    document.getElementById("winButton").setAttribute("style", "visibility: visible;");
-    document.getElementById("AL").setAttribute("style","visibility:hidden;");
-    document.getElementById("MZ").setAttribute("style","visibility:hidden;");
-    over = true;
-  }
-  if (strikes === 6){
-    youLose();
-    over = true;
-  }
-}
-
 function getWord() {
   var x = (1 + Math.floor(Math.random() * (wordList.length - 1)));
   newWord = wordList[x].toUpperCase();
   for (i = 0; i < newWord.length; i++) {
-    document.getElementById("word").innerHTML += "<td id=\'" +
-      i + "\' class=\'hiddenLetter\'>" + newWord[i] + "</td>";
+    if (alphabet.includes(newWord[i])) {
+      document.getElementById("word").innerHTML += "<td id=\'" +
+        i + "\' class=\'hiddenLetter\'>" + newWord[i].toUpperCase() + "</td>";
+    } else {
+      document.getElementById("word").innerHTML += "<td class=\'hiddenPunc\'>" + newWord[i] + "</td>";
+    }
   }
 }
+
+function guess(letter) {
+  if (!guessedArray.includes(letter)) {
+    var guessed = [];
+    var gotOne = false;
+    for (i = 0; i < newWord.length; i++) {
+      if (letter === newWord[i]) {
+        document.getElementById(i).setAttribute("style", "color:black;");
+        gotOne = true;
+      }
+    }
+    if (!gotOne) {
+      document.getElementById(bodyParts[strikes]).setAttribute("style", "color:black;");
+      strikes += 1;
+    }
+    for (j = 0; j < newWord.length; j++) {
+      if (!document.getElementById(j).hasAttribute("style")) {
+        guessed.push(false);
+      }
+    }
+    if (guessed.length === 0) {
+      document.getElementById("winButton").setAttribute("style", "visibility: visible;");
+      document.getElementById("AL").setAttribute("style", "visibility:hidden;");
+      document.getElementById("MZ").setAttribute("style", "visibility:hidden;");
+      over = true;
+    }
+    if (strikes === 6) {
+      youLose();
+      over = true;
+    }
+  }
+  guessedArray.push(letter);
+  for (i = 0; i < guessedArray.length; i++) {
+    document.getElementById(guessedArray[i]).setAttribute("style", "opacity:0;");
+    document.getElementById(guessedArray[i]).setAttribute("onclick", "");
+  }
+
+}
+
 
 function winner() {
   document.getElementById("word").innerHTML = "";
@@ -65,12 +78,13 @@ function winner() {
   getWord();
   document.getElementById("winButton").setAttribute("style", "visibility:hidden;");
   document.getElementById("loseButton").setAttribute("style", "visibility:hidden;");
-  document.getElementById("AL").setAttribute("style","visibility:visible;");
-  document.getElementById("MZ").setAttribute("style","visibility:visible;");
+  document.getElementById("AL").setAttribute("style", "visibility:visible;");
+  document.getElementById("MZ").setAttribute("style", "visibility:visible;");
   strikes = 0;
-  for (i = 0; i < bodyParts.length; i ++){
-    document.getElementById(bodyParts[i]).setAttribute("style","color:white;");
+  for (i = 0; i < bodyParts.length; i++) {
+    document.getElementById(bodyParts[i]).setAttribute("style", "color:white;");
   }
+  guessedArray = [];
   over = false;
 }
 
@@ -82,12 +96,12 @@ document.addEventListener("DOMContentLoaded", function() {
 function youLose() {
   for (j = 0; j < newWord.length; j++) {
     if (!document.getElementById(j).hasAttribute("style")) {
-      document.getElementById(j).setAttribute("style","color:red;");
+      document.getElementById(j).setAttribute("style", "color:red;");
     }
   }
   document.getElementById("loseButton").setAttribute("style", "visibility: visible;");
-  document.getElementById("AL").setAttribute("style","visibility:hidden;");
-  document.getElementById("MZ").setAttribute("style","visibility:hidden;");
+  document.getElementById("AL").setAttribute("style", "visibility:hidden;");
+  document.getElementById("MZ").setAttribute("style", "visibility:hidden;");
 }
 
 window.addEventListener("keypress", keyListen, false);

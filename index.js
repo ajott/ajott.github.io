d11a107b3945d48d56f61e444d5d81ee1991aa46
ajott <ajott@mtu.edu>
2017-08-10 10:49:03 -0400

    MainIndex - BSOD

---
+++ index.js
@@ -0,0 +1,15 @@
+function requestFullScreen() {
+    var element = document.getElementById('try');
+    element.innerHTML = "<img src=\"BSODFull.png\">";
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}

c41d3aaf3a35297689925fce0f8121c4a026f31a
ajott <ajott@mtu.edu>
2018-11-05 13:38:08 -0500

    QuickRef 0.2
    Updated with more tables. Roughly 50% of content is populated... 30% if
    adding crit tables

--- quickref/main.js
+++ quickref/main.js
@@ -1,17 +1,32 @@
-$("#gearTable").stupidtable();
-
-
 function togglePanel(panel) {
-    var panels = ['#combat','#gear','#weapons','#armour'];
-    var tabs = ['#combatTab','#gearTab', '#weaponTab','#armourTab'];
-  
+    var panels = ['#combat', '#gear', '#weapons', '#armour', "#weaponQual", "#weaponMods", "#tools", "#armourMods", "#psyPowers","#psyPhenom","#psyPerils","#ammo"];
+    var tabs = ['#combatTab', '#gearTab', '#weaponTab', '#armourTab', "#weaponTab", "#weaponTab", "#gearTab", "#armourTab", "#psykerTab", "#psykerTab", "#psykerTab", "#weaponTab"];
+
     for (i = 0; i < panels.length; i++) {
-      if (i == panel) {
-        $(panels[i]).show();
-        $(tabs[i]).addClass("btn-info");
-      } else {
         $(panels[i]).hide();
         $(tabs[i]).removeClass("btn-info");
-      }
     }
-  }
+    $(panels[panel]).show();
+    $(tabs[panel]).addClass("btn-info");
+}
+
+// function loadFile(filePath) {
+//     var result = null;
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.open("GET", filePath, false);
+//     xmlhttp.send();
+//     if (xmlhttp.status == 200) {
+//         result = xmlhttp.responseText;
+//     }
+//     return result;
+// }
+
+// function importTables() {
+//     var tables = ['#combat', '#gear', '#weapons', '#armour', "#weaponQual", "#weaponMods", "#tools", "#armourMods"];
+//     var file = ["combat","gear","weapons","armour","weaponQual","weaponMods","tools","armourMods"]
+    
+//     for (i=0; i<tables.length; i++){
+//         $(tables[i]).html(loadFile("tables/"&file[i]&".txt"));
+//     }
+    
+// }

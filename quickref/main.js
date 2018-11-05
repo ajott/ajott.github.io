6ee853466916478fdc3f70b355f1c51a843b508c
ajott <ajott@mtu.edu>
2018-11-05 11:23:06 -0500

    QuickRef 0.1
    Gave up on table plugins. Manually loading tables, using bootstrap. Have
    CA, gear, weapons, armour so far

--- quickref/main.js
+++ quickref/main.js
@@ -1,8 +1,17 @@
-$('#my-final-table').dynatable({
-    dataset: {
-        ajax: true,
-        ajaxUrl: "gear.json",
-        ajaxOnLoad: true,
-        records: []
+$("#gearTable").stupidtable();
+
+
+function togglePanel(panel) {
+    var panels = ['#combat','#gear','#weapons','#armour'];
+    var tabs = ['#combatTab','#gearTab', '#weaponTab','#armourTab'];
+  
+    for (i = 0; i < panels.length; i++) {
+      if (i == panel) {
+        $(panels[i]).show();
+        $(tabs[i]).addClass("btn-info");
+      } else {
+        $(panels[i]).hide();
+        $(tabs[i]).removeClass("btn-info");
+      }
     }
-});
+  }

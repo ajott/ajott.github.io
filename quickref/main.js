a7c6935154f2cda91c2d1c218dacecd6e180fd22
ajott <ajott@mtu.edu>
2018-11-05 15:14:02 -0500

    QuickRef 0.3
    Added more tables, exp costs, formatting.
    #TODO: Finish character tables, add crit tables.

--- quickref/main.js
+++ quickref/main.js
@@ -1,14 +1,17 @@
 function togglePanel(panel) {
-    var panels = ['#combat', '#gear', '#weapons', '#armour', "#weaponQual", "#weaponMods", "#tools", "#armourMods", "#psyPowers","#psyPhenom","#psyPerils","#ammo"];
-    var tabs = ['#combatTab', '#gearTab', '#weaponTab', '#armourTab', "#weaponTab", "#weaponTab", "#gearTab", "#armourTab", "#psykerTab", "#psykerTab", "#psykerTab", "#weaponTab"];
+    var panels = ['#combat', '#gear', '#weapons', '#armour', "#weaponQual", "#weaponMods", "#tools",
+     "#armourMods", "#psyPowers","#psyPhenom","#psyPerils","#ammo", "#skills","#talents","#traits","#mutations","#maligna","#shock","#exp"];
+    var tabs = ['#combatTab', '#gearTab', '#weaponTab', '#armourTab', "#weaponTab", "#weaponTab", "#gearTab",
+     "#armourTab", "#psykerTab", "#psykerTab", "#psykerTab", "#weaponTab", "#charTab", "#charTab", "#charTab", "#charTab", "#charTab", "#charTab", "#expTab"];
 
     for (i = 0; i < panels.length; i++) {
         $(panels[i]).hide();
-        $(tabs[i]).removeClass("btn-info");
+        $(tabs[i]).removeClass("btn-danger");
     }
     $(panels[panel]).show();
-    $(tabs[panel]).addClass("btn-info");
+    $(tabs[panel]).addClass("btn-danger");
 }
+
 
 // function loadFile(filePath) {
 //     var result = null;

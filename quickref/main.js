93a16b8eac23e6ae41e39883c8746975e21005bc
ajott <ajott@mtu.edu>
2018-11-06 09:17:00 -0500

    QuickRef 0.4 (RC1)
    Finished crit tables, character tables, some formatting. Ready for
    initial release.

--- quickref/main.js
+++ quickref/main.js
@@ -1,8 +1,10 @@
 function togglePanel(panel) {
     var panels = ['#combat', '#gear', '#weapons', '#armour', "#weaponQual", "#weaponMods", "#tools",
-     "#armourMods", "#psyPowers","#psyPhenom","#psyPerils","#ammo", "#skills","#talents","#traits","#mutations","#maligna","#shock","#exp"];
+     "#armourMods", "#psyPowers","#psyPhenom","#psyPerils","#ammo", "#skills","#talents","#traits",
+     "#mutations","#maligna","#shock","#exp","#energyCrit","#explosiveCrit","#impactCrit","#rendingCrit","#cybernetics","#consumables"];
     var tabs = ['#combatTab', '#gearTab', '#weaponTab', '#armourTab', "#weaponTab", "#weaponTab", "#gearTab",
-     "#armourTab", "#psykerTab", "#psykerTab", "#psykerTab", "#weaponTab", "#charTab", "#charTab", "#charTab", "#charTab", "#charTab", "#charTab", "#expTab"];
+     "#armourTab", "#psykerTab", "#psykerTab", "#psykerTab", "#weaponTab", "#charTab", "#charTab", "#charTab", 
+     "#charTab", "#charTab", "#charTab", "#expTab","#critTab","#critTab","#critTab","#critTab","#gearTab","#gearTab"];
 
     for (i = 0; i < panels.length; i++) {
         $(panels[i]).hide();
@@ -11,25 +13,3 @@ function togglePanel(panel) {
     $(panels[panel]).show();
     $(tabs[panel]).addClass("btn-danger");
 }
-
-
-// function loadFile(filePath) {
-//     var result = null;
-//     var xmlhttp = new XMLHttpRequest();
-//     xmlhttp.open("GET", filePath, false);
-//     xmlhttp.send();
-//     if (xmlhttp.status == 200) {
-//         result = xmlhttp.responseText;
-//     }
-//     return result;
-// }
-
-// function importTables() {
-//     var tables = ['#combat', '#gear', '#weapons', '#armour', "#weaponQual", "#weaponMods", "#tools", "#armourMods"];
-//     var file = ["combat","gear","weapons","armour","weaponQual","weaponMods","tools","armourMods"]
-    
-//     for (i=0; i<tables.length; i++){
-//         $(tables[i]).html(loadFile("tables/"&file[i]&".txt"));
-//     }
-    
-// }

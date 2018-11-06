45eb8014d5029deb26a53a971541c6768eadc530
ajott <ajott@mtu.edu>
2018-11-06 09:30:31 -0500

    QuickRef 0.5
    Added homeworlds, backgrounds, roles, and divinations

--- quickref/main.js
+++ quickref/main.js
@@ -1,10 +1,12 @@
 function togglePanel(panel) {
     var panels = ['#combat', '#gear', '#weapons', '#armour', "#weaponQual", "#weaponMods", "#tools",
      "#armourMods", "#psyPowers","#psyPhenom","#psyPerils","#ammo", "#skills","#talents","#traits",
-     "#mutations","#maligna","#shock","#exp","#energyCrit","#explosiveCrit","#impactCrit","#rendingCrit","#cybernetics","#consumables"];
+     "#mutations","#maligna","#shock","#exp","#energyCrit","#explosiveCrit","#impactCrit","#rendingCrit","#cybernetics","#consumables",
+    "#homeworlds","#backgrounds","#roles","#divinations"];
     var tabs = ['#combatTab', '#gearTab', '#weaponTab', '#armourTab', "#weaponTab", "#weaponTab", "#gearTab",
      "#armourTab", "#psykerTab", "#psykerTab", "#psykerTab", "#weaponTab", "#charTab", "#charTab", "#charTab", 
-     "#charTab", "#charTab", "#charTab", "#expTab","#critTab","#critTab","#critTab","#critTab","#gearTab","#gearTab"];
+     "#charTab", "#charTab", "#charTab", "#expTab","#critTab","#critTab","#critTab","#critTab","#gearTab","#gearTab",
+     "#charTab", "#charTab", "#charTab", "#charTab"];
 
     for (i = 0; i < panels.length; i++) {
         $(panels[i]).hide();

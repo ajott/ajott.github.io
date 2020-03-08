10b5e103d6b6b201bc6e232c7be514f74a19a0a0
Alexander Ott <ajott@mtu.edu>
2020-03-08 10:37:11 -0400

    d&d Ref 0.7.5.6.4
    Added "Zone of unTruth" at Brendan's request.

--- dndref/spells.js
+++ dndref/spells.js
@@ -8802,6 +8802,22 @@ var spell = [{
             "",
             "An affected creature is aware of the spell and can thus avoid answering questions to which it would normally respond with a lie. Such creatures can be evasive in its answers as long as it remains within the boundaries of the truth."
         ]
+    },
+    {
+        "name": "Zone of unTruth",
+        "level": "2nd level",
+        "school": "Enchantment",
+        "time": "1 action",
+        "homebrew": "YES",
+        "range": "60 feet",
+        "components": "V, S, M",
+        "duration": "10 minutes",
+        "classes": "Bard, Cleric, Paladin, Paladin (Devotion), Paladin (Crown)",
+        "text": [
+            "You create a magical zone that guards against the truth in a 15-foot-radius sphere centered on a point of your choice within range. Until the spell ends, a creature that enters the spell's area for the first time on a turn or starts its turn there must make a Charisma saving throw. On a failed save, a creature can't speak the truth while in the radius. You know whether each creature succeeds or fails on its saving throw.",
+            "",
+            "An affected creature is aware of the spell, but not aware of who cast it, and can thus avoid answering questions to which it would normally respond with the truth. Such creatures can be evasive in its answers as long as it remains outside the boundaries of the truth."
+        ]
     }
 ];
 

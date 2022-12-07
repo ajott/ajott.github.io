f9a965de79d362d9009242c97362a87f77935ddc
Alexander Ott <ajott@mtu.edu>
2022-12-07 15:07:27 -0500

    Gallery Revamp

--- pics/Lux2022/main.js
+++ pics/Lux2022/main.js
@@ -1,3 +1,8 @@
-function expando(el) {
-    
+function accordion(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        $('#'+id).removeClass("w3-hide").addClass("w3-show")
+    } else {
+        $('#'+id).removeClass("w3-show").addClass("w3-hide")
+    }
 }
